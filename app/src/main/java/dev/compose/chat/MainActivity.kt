f566492584b8b1a884cc04860440dc4fd57da0e4
Juan Vilada <jvillad1@gmail.com>
2021-03-26 11:14:19 -0500

    feat: Compose AS template

---
+++ app/src/main/java/dev/compose/chat/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.compose.chat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.compose.chat.ui.theme.ChatposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChatposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ChatposeTheme {
+        Greeting("Android")
+    }
+}
